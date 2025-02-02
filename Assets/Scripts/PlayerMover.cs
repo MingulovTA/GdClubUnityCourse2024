12b5ed36dc6c46521b4f92c950cfd61196004fa4
MingulovTA <mingulov.t@yandex.ru>
2025-02-02 15:25:28 +0700

    feat: Lesson 11. Description of control logic

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -4,19 +4,15 @@ using UnityEngine;
 public class PlayerMover : MonoBehaviour
 {
     private const string AXIS_HORISONTAL = "Horizontal";
-    private const string AXIS_VERTICAL = "Vertical";
     
     [SerializeField] private Rigidbody2D _rigidbody2D;
     [SerializeField] private float _speed = 500f;
 
     private Vector2 _velocity;
 
-    private void FixedUpdate()
-    {
-        _velocity.x = Input.GetAxis(AXIS_HORISONTAL);
-        _velocity.y = Input.GetAxis(AXIS_VERTICAL);
-        _rigidbody2D.velocity = (_velocity*_speed*Time.fixedDeltaTime+Physics2D.gravity);
-    }
+    private void Update()=>_velocity.x = Input.GetAxis(AXIS_HORISONTAL);
+
+    private void FixedUpdate()=>_rigidbody2D.velocity = _velocity*_speed*Time.fixedDeltaTime+Physics2D.gravity;
 
     private void OnValidate() => _rigidbody2D = GetComponent<Rigidbody2D>();
 }
