a09aa31cb0a00f1233759d9b90d949b4e457ea66
MingulovTA <mingulov.t@yandex.ru>
2025-02-02 15:05:14 +0700

    feat: Lesson 9. Experiment with a character

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -7,14 +7,15 @@ public class PlayerMover : MonoBehaviour
     private const string AXIS_VERTICAL = "Vertical";
     
     [SerializeField] private Rigidbody2D _rigidbody2D;
+    [SerializeField] private float _speed = 10f;
 
     private Vector2 _velocity;
 
-    private void Update()
+    private void FixedUpdate()
     {
         _velocity.x = Input.GetAxis(AXIS_HORISONTAL);
         _velocity.y = Input.GetAxis(AXIS_VERTICAL);
-        _rigidbody2D.velocity = _velocity;
+        _rigidbody2D.velocity = _velocity*_speed+Physics2D.gravity;
     }
 
     private void OnValidate() => _rigidbody2D = GetComponent<Rigidbody2D>();
